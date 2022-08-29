87e7666198f0692e67a36c2b9c737c6b9d0a379d
Calvin Munene <96726493+MuneneCalvin@users.noreply.github.com>
2022-08-29 09:32:56 +0300

    Simple shell

--- hsh.h
+++ hsh.h
@@ -51,107 +51,107 @@ extern char **environ;
 
 
 /**
- *  * struct liststr - singly linked list
- *   * @num: the number field
- *    * @str: a string
- *     * @next: points to the next node
- *      */
+ * struct liststr - singly linked list
+ * @num: the number field
+ * @str: a string
+ * @next: points to the next node
+ */
 typedef struct liststr
 {
-		int num;
-			char *str;
-				struct liststr *next;
+	int num;
+	char *str;
+	struct liststr *next;
 } list_t;
 
 /**
- *  *struct passinfo - contains pseudo-arguements to pass into a function,
- *   *					allowing uniform prototype for function pointer struct
- *    * @arg: a string generated from getline containing arguements
- *     * @argv: an array of strings generated from arg
- *      * @path: a string path for the current command
- *       * @argc: the argument count
- *        * @line_count: the error count
- *         * @err_num: the error code for exit()s
- *          * @linecount_flag: if on count this line of input
- *           * @fname: the program filename
- *            * @env: linked list local copy of environ
- *             * @environ: custom modified copy of environ from LL env
- *              * @history: the history node
- *               * @alias: the alias node
- *                * @env_changed: on if environ was changed
- *                 * @status: the return status of the last exec'd command
- *                  * @cmd_buf: address of pointer to cmd_buf, on if chaining
- *                   * @cmd_buf_type: CMD_type ||, &&, ;
- *                    * @readfd: the fd from which to read line input
- *                     * @histcount: the history line number count
- *                      * @left_redirect_from_fd: the fd to left redirect from
- *                       * @left_append: true if heredoc
- *                        * @right_redirect_from_fd: fd right redirecting from (default 1)
- *                         * @right_redirect_to_fd: fd right redirecting to
- *                          * @right_append: true if right stream appends
- *                           * @heredoc: value of HEREDOC delimeter
- *                            * @heredoc_txt: accumulated HEREDOC lines
- *                             * @heredoc_cmd: the command to pipe HEREDOC line
- *                              * @help: help flags
- *                               * @pipefd: fd pipe for interprocess communication | pipe
- *                                * @startup_fd: fd of startup file or -1
- *                                 * @dup_stdin: saved duplicate of stdin for restoration after redirect
- *                                  * @dup_stdout: saved duplicate of stdout for restoration after redirect
- *                                   */
+ *struct passinfo - contains pseudo-arguements to pass into a function,
+ *					allowing uniform prototype for function pointer struct
+ * @arg: a string generated from getline containing arguements
+ * @argv: an array of strings generated from arg
+ * @path: a string path for the current command
+ * @argc: the argument count
+ * @line_count: the error count
+ * @err_num: the error code for exit()s
+ * @linecount_flag: if on count this line of input
+ * @fname: the program filename
+ * @env: linked list local copy of environ
+ * @environ: custom modified copy of environ from LL env
+ * @history: the history node
+ * @alias: the alias node
+ * @env_changed: on if environ was changed
+ * @status: the return status of the last exec'd command
+ * @cmd_buf: address of pointer to cmd_buf, on if chaining
+ * @cmd_buf_type: CMD_type ||, &&, ;
+ * @readfd: the fd from which to read line input
+ * @histcount: the history line number count
+ * @left_redirect_from_fd: the fd to left redirect from
+ * @left_append: true if heredoc
+ * @right_redirect_from_fd: fd right redirecting from (default 1)
+ * @right_redirect_to_fd: fd right redirecting to
+ * @right_append: true if right stream appends
+ * @heredoc: value of HEREDOC delimeter
+ * @heredoc_txt: accumulated HEREDOC lines
+ * @heredoc_cmd: the command to pipe HEREDOC line
+ * @help: help flags
+ * @pipefd: fd pipe for interprocess communication | pipe
+ * @startup_fd: fd of startup file or -1
+ * @dup_stdin: saved duplicate of stdin for restoration after redirect
+ * @dup_stdout: saved duplicate of stdout for restoration after redirect
+ */
 typedef struct passinfo
 {
-		char *arg;
-			char **argv;
-				char *path;
-					int argc;
-						unsigned int line_count;
-							int err_num;
-								int linecount_flag;
-									char *fname;
-										list_t *env;
-											list_t *history;
-												list_t *alias;
-													char **environ;
-														int env_changed;
-															int status;
+	char *arg;
+	char **argv;
+	char *path;
+	int argc;
+	unsigned int line_count;
+	int err_num;
+	int linecount_flag;
+	char *fname;
+	list_t *env;
+	list_t *history;
+	list_t *alias;
+	char **environ;
+	int env_changed;
+	int status;
 
-																char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
-																	int cmd_buf_type; /* CMD_type ||, &&, ; */
-																		int readfd;
-																			int histcount;
+	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
+	int cmd_buf_type; /* CMD_type ||, &&, ; */
+	int readfd;
+	int histcount;
 
-																				int left_redirect_from_fd;
-																					int left_append;
+	int left_redirect_from_fd;
+	int left_append;
 
-																						int right_redirect_from_fd;
-																							int right_redirect_to_fd;
-																								int right_append;
+	int right_redirect_from_fd;
+	int right_redirect_to_fd;
+	int right_append;
 
-																									char *heredoc;
-																										char *heredoc_txt;
-																											char *heredoc_cmd;
-																												char *help;
+	char *heredoc;
+	char *heredoc_txt;
+	char *heredoc_cmd;
+	char *help;
 
-																													int pipefd[2];
-																														int startup_fd;
-																															int dup_stdin;
-																																int dup_stdout;
+	int pipefd[2];
+	int startup_fd;
+	int dup_stdin;
+	int dup_stdout;
 
 } info_t;
 
 #define INFO_INIT \
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
-		0, 0, 0, -1, 0, 1, -1, 0, NULL, NULL, NULL, NULL, {0}, -1, 0, 0}
+	0, 0, 0, -1, 0, 1, -1, 0, NULL, NULL, NULL, NULL, {0}, -1, 0, 0}
 
 /**
- *  *struct builtin - contains a builtin string and related function
- *   *@type: the builtin command flag
- *    *@func: the function
- *     */
+ *struct builtin - contains a builtin string and related function
+ *@type: the builtin command flag
+ *@func: the function
+ */
 typedef struct builtin
 {
-		char *type;
-			int (*func)(info_t *);
+	char *type;
+	int (*func)(info_t *);
 } builtin_table;
 
 
